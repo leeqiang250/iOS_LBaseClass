84bf752a98a6219c49964a607cd1f145f9197835
leeqiang250 <leeqiang250@163.com>
2018-01-10 11:54:03 +0800

    d

--- LBaseClass/LBaseClass/LDao/LDB.mm
+++ LBaseClass/LBaseClass/LDao/LDB.mm
@@ -8,22 +8,22 @@
 
 #import "LDB.h"
 
-#import <WCDB/WCDB.h>
-#import "LObj.h"
+//#import <WCDB/WCDB.h>
 
 @implementation LDB
 
 - (void)dd {
-    LObj * obj = [[LObj alloc] init];
-    obj.version = [NSUUID UUID].UUIDString;
-    obj.displayOrder = [NSUUID UUID].UUIDString;
-    NSString * path = [NSTemporaryDirectory() stringByAppendingPathComponent:@"tmp.db"];
-    WCTDatabase * wcdb = [[WCTDatabase alloc] initWithPath:path];
-    [wcdb createTableAndIndexesOfName:NSStringFromClass(LObj.class) withClass:LObj.class];
-    [wcdb insertObject:obj into:NSStringFromClass(LObj.class)];
-    
-    NSArray * ddd =  [wcdb getAllObjectsOfClass:LObj.class fromTable:NSStringFromClass(LObj.class)];
-    NSLog(@"%@", NSStringFromSelector(_cmd));
+//    LObj2 * obj = [[LObj2 alloc] init];
+//    obj.version = [NSUUID UUID].UUIDString;
+//    obj.displayOrder = [NSUUID UUID].UUIDString;
+//    obj.aaaa = [NSUUID UUID].UUIDString;
+//    NSString * path = [NSTemporaryDirectory() stringByAppendingPathComponent:@"tmp.db"];
+//    WCTDatabase * wcdb = [[WCTDatabase alloc] initWithPath:path];
+//    [wcdb createTableAndIndexesOfName:NSStringFromClass(LObj2.class) withClass:LObj2.class];
+//    [wcdb insertObject:obj into:NSStringFromClass(LObj2.class)];
+//
+//    NSArray * ddd =  [wcdb getAllObjectsOfClass:LObj2.class fromTable:NSStringFromClass(LObj2.class)];
+//    NSLog(@"%@", NSStringFromSelector(_cmd));
 }
 
 @end
